1d6abc43be11d431886479e1b57a3d488fbae112
Ruben O. <ursa.mugler@gmail.com>
2025-08-24 05:11:51 -0600

    more stuff

--- src/App.tsx
+++ src/App.tsx
@@ -262,19 +262,6 @@ const CoinFlipApp: React.FC = () => {
           </div>
         </div>
       </div>
-
-      <style jsx>{`
-        @keyframes fadeInUp {
-          from {
-            opacity: 0;
-            transform: translateY(20px);
-          }
-          to {
-            opacity: 1;
-            transform: translateY(0);
-          }
-        }
-      `}</style>
     </div>
   );
 };
